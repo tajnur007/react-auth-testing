8cf114fd8db5be01d331e3106a37e885a7a6fbad
Kazi Tajnur Islam <kazi.tajnur@vivacomsolutions.com>
2022-11-08 12:16:50 +0600

    Basic login structure made with ant design components

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FC, FormEvent, useState } from 'react';
+import { Button, Checkbox, Form, Input } from 'antd';
 
-function App() {
+import './App.css';
+import 'antd/dist/antd.css';
+
+const App: FC = () => {
+  const [user, setUser] = useState(null);
+
+  const handleOnFinish = (values: any) => {
+    console.log('Success:', values);
+    setUser(values);
+  };
+
+  const handleOnFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  const handleLogout = () => {
+    setUser(null);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {!user ? (
+          <Form
+            name="basic"
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 16 }}
+            initialValues={{ remember: true }}
+            onFinish={handleOnFinish}
+            onFinishFailed={handleOnFinishFailed}
+            autoComplete="off"
+          >
+            <Form.Item
+              label="Username"
+              name="username"
+              rules={[{ required: true, message: 'Please input your username!' }]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[{ required: true, message: 'Please input your password!' }]}
+            >
+              <Input.Password />
+            </Form.Item>
+
+            <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+
+            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+              <Button type="primary" htmlType="submit">
+                Login
+              </Button>
+            </Form.Item>
+          </Form>
+        ) : (
+          <div>
+            <Button type="primary" onClick={handleLogout}>
+              Logout
+            </Button>
+          </div>
+        )}
       </header>
     </div>
   );
