13c4850ff318ce37eca3bd34ce9f8960c70ca10b
Kazi Tajnur Islam <kazi.tajnur@vivacomsolutions.com>
2022-11-08 17:59:45 +0600

    Custom validation demo added

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
-import { FC, FormEvent, useState } from 'react';
-import { Button, Checkbox, Form, Input } from 'antd';
+import { FC, useEffect, useState } from 'react';
+import { Button, Checkbox, Form, Input, Space, Spin } from 'antd';
 
 import './App.css';
 import 'antd/dist/antd.css';
 
+interface UserDataType {
+  email: string;
+  password: string;
+  remember: boolean;
+}
+
 const App: FC = () => {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState<UserDataType | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [isDisable, setIsDisable] = useState<boolean>(true);
+
+  useEffect(() => {
+    const credentials = localStorage.getItem('credentials');
+    credentials ? setUser(JSON.parse(credentials)) : setUser(null);
+    setIsLoading(false);
+    console.log('credentials', credentials);
+  }, []);
 
   const handleOnFinish = (values: any) => {
     console.log('Success:', values);
     setUser(values);
+    localStorage.setItem('credentials', JSON.stringify(values));
   };
 
   const handleOnFinishFailed = (errorInfo: any) => {
@@ -18,6 +34,28 @@ const App: FC = () => {
 
   const handleLogout = () => {
     setUser(null);
+    localStorage.removeItem('credentials');
+  }
+
+  const handleValidation = async (_: any, value: string) => {
+    if (!value.length) {
+      return Promise.resolve();
+    }
+    if ((!value.includes('@') || !value.includes('.'))) {
+      return Promise.reject(new Error('Enter a valid email'));
+    }
+  }
+
+  if (isLoading) {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Space size="middle">
+            <Spin size="large" />
+          </Space>
+        </header>
+      </div>
+    )
   }
 
   return (
@@ -34,10 +72,16 @@ const App: FC = () => {
             autoComplete="off"
           >
             <Form.Item
-              label="Username"
-              name="username"
-              rules={[{ required: true, message: 'Please input your username!' }]}
+              label="Email"
+              name="email"
+              rules={[
+                { required: true, message: 'Please input your email' },
+                { validator: handleValidation }
+                // { type: 'email', message: 'Enter a valid email' },
+                // { pattern: /taj/, message: 'Must include "taj"', warningOnly: true, }
+              ]}
             >
+              {/* <Input onChange={(e) => setIsDisable(!(/taj/.test(e.target.value)))} /> */}
               <Input />
             </Form.Item>
 
@@ -54,13 +98,18 @@ const App: FC = () => {
             </Form.Item>
 
             <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-              <Button type="primary" htmlType="submit">
+              <Button type="primary" htmlType="submit" disabled={isDisable}>
                 Login
               </Button>
             </Form.Item>
           </Form>
         ) : (
           <div>
+            <div>
+              User email: {user?.email} <br />
+              Password: {user.password}
+            </div>
+
             <Button type="primary" onClick={handleLogout}>
               Logout
             </Button>
